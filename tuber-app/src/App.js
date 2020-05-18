1b8732f70705a744468dc456792acd99ba3a3088
AceDroo <deroom01@gmail.com>
2020-05-18 23:06:55 +0000

    Merges 1-home-screen into master

---
+++ tuber-app/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+import './App.css';
+
+const mapStyles = {
+  width: '95%',
+  height: '75%',
+};
+
+class App extends Component {
+  render() {
+      return (
+      <div className="App">
+        <h1 className = "App-header">Tuber</h1>
+        <div className = "App-map">
+          <Map
+            google={this.props.google}
+            zoom = {8}
+            style = {mapStyles}
+            initialCenter={{ lat:-34.87852, lng:150.59510}}
+            />
+        </div>
+        <div>
+          <button type="button">Home</button>
+          <button type="button">History</button> 
+          <button type="button">Account</button> 
+        </div>
+      </div>
+   );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBRHWRWz4ALveAF2bcLkry0hL5UFNmC2fo'
+})(App);
