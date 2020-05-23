6fbfe20019763031660ce87bf56df3093755c25f
AceDroo <deroom01@gmail.com>
2020-05-23 04:47:05 +0000

    Merges 2-Adds-Markers with master

--- ClientApp/src/Main.js
+++ ClientApp/src/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 import './Main.css';
+import CurrentLocation from './Map';
 
 const mapStyles = {
   width: '95%',
@@ -8,17 +9,80 @@ const mapStyles = {
 };
 
 class Main extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+      locations: [
+        {name: 'Wollongong Art Gallery', latitude: -34.4209237, longitude: 150.8930583},
+        {name: 'Wollongong Botanic Gardens', latitude: -34.4170295, longitude: 150.887136},
+        {name: 'Statue of Liberty Historical Monument', latitude: 40.6892534, longitude: -74.0466891},
+        {name: 'Lincoln Memorial', latitude: 38.8885222, longitude: -77.0191213},
+        {name: 'Big Ben', latitude: 51.5007325, longitude: -0.1268141},
+        {name: 'Bens Walk, Nowra', latitude: -34.8716197, longitude: 150.589701}
+      ]
+    }
+  }
+
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
+  displayMarkers = () => {
+    return this.state.locations.map((location, index) => {
+      return <Marker 
+        name={location.name} 
+        key={index} 
+        id={index} 
+        position ={{
+          lat: location.latitude,
+          lng: location.longitude
+        }}
+        onClick={this.onMarkerClick} 
+      />
+    })
+  }
+
   render() {
+      if (!this.props.loaded) {
+        return <div className = "App">Loading...</div>
+      }
       return (
       <div className="App">
         <h1 className = "App-header">Tuber</h1>
         <div className = "App-map">
-          <Map
+          <CurrentLocation
+            centerAroundCurrentLocation
             google={this.props.google}
-            zoom = {8}
-            style = {mapStyles}
-            initialCenter={{ lat:-34.87852, lng:150.59510}}
-            />
+          >
+            {this.displayMarkers()}
+            <Marker onClick={this.onMarkerClick} name="Current Location"/>
+            <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}
+              onClose={this.onClose}
+            >
+            <div>
+              <h4>{this.state.selectedPlace.name}</h4>
+            </div>
+            </InfoWindow>
+          </CurrentLocation>
         </div>
         <div>
           <button type="button">Home</button>
