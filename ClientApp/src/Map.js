446fcfdd151abbd3c422b72b8027ba7d11829188
AceDroo <deroom01@gmail.com>
2020-05-25 02:57:27 +0000

    Revert "Adds Register Screen"
    This reverts commit 1007afe523d1e2309bf7d22b28e5d62639ea4293.

--- ClientApp/src/Map.js
+++ ClientApp/src/Map.js
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom';
 const mapStyles = {
     map: {
         position: 'absolute',
-        width: '100%',
-        height: '100%'
+        width: '40%',
+        height: '70%'
     }
 };
 
