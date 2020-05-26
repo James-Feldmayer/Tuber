46c2b195ab0d826059d9bc88ddc509dcae8f874c
the jukebox <grimshawjoshua2@gmail.com>
2020-05-26 21:12:29 +1000

    Restructures project to clean up root directory

--- tuber/ClientApp/src/index.tsx
+++ tuber/ClientApp/src/index.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ReactDOM from 'react-dom';
 import Login from './Login';
-import Main from './Main'
+import Main from './Main';
+import Register from './Register';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Main />
+    <Router>
+      <Switch>
+        <Route path="/Login">
+          <Login />
+        </Route>
+        <Route path="/Register">
+          <Register />
+        </Route>
+        <Route path="/">
+          <Main />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
